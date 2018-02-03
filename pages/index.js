4f795866c79e947e597f6ee6d012b5b91ca859de
Jade Pennig <jade@pennig.name>
2018-02-03 13:09:41 -0600

    Add google analytics

--- pages/index.js
+++ pages/index.js
@@ -2,89 +2,128 @@ import { Component } from 'react'
 import { times } from 'lodash'
 import Head from 'next/head'
 import Masonry from 'react-masonry-component'
+import ReactGA from 'react-ga'
 
 export default class ChickenWaffles extends Component {
-	render() {
-		return (
-			<div className="container">
-				<Head>
-					<link href="https://fonts.googleapis.com/css?family=Bubblegum+Sans" rel="stylesheet" />
-				</Head>
+  componentDidMount() {
+    ReactGA.initialize('UA-113511865-1')
+    ReactGA.pageview('/')
+  }
 
-				<div className="header">
-					<h1>Chicken and Waffle!</h1>
+  render() {
+    return (
+      <div className="container">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Bubblegum+Sans"
+            rel="stylesheet"
+          />
+        </Head>
 
-					<div className="chicken-waffles">
-						<img className="chicken" src="/static/chicken.png" alt="Chicken" width="300" />
-						<img className="waffle" src="/static/waffle.png" alt="Waffle" width="300" />
-					</div>
-				</div>
+        <div className="header">
+          <h1>Chicken and Waffle!</h1>
 
-				<div className="masonry">
-					<Masonry elementType="ul" options={{fitWidth: true}}>
-						{times(9, i => (
-							<li key={i} style={{listStyle: 'none'}}>
-								<img width="400px" src={`/static/cook/${i + 1}.jpg`} alt='Making Chicken and Waffles' />
-							</li>
-						))}
-					</Masonry>
+          <div className="chicken-waffles">
+            <img
+              className="chicken"
+              src="/static/chicken.png"
+              alt="Chicken"
+              width="300"
+            />
+            <img
+              className="waffle"
+              src="/static/waffle.png"
+              alt="Waffle"
+              width="300"
+            />
+          </div>
+        </div>
 
-					<img src="/static/cook/10.jpg" width="100%" alt="Making Chicken and Waffles" />
-				</div>
+        <div className="masonry">
+          <Masonry elementType="ul">
+            {times(9, i => (
+              <li key={i} style={{ listStyle: 'none' }}>
+                <img
+                  src={`/static/cook/${i + 1}.jpg`}
+                  alt="Making Chicken and Waffles"
+                  style={{ width: '400px' }}
+                />
+              </li>
+            ))}
+          </Masonry>
 
-				<style jsx global>{`
-					* { margin: 0; padding: 0; }
+          <img
+            src="/static/cook/10.jpg"
+            width="100%"
+            alt="Making Chicken and Waffles"
+          />
+        </div>
 
-					body {
-						font-family: 'Bubblegum Sans', sans-serif;
-						text-align: center;
-					}
+        <style jsx global>{`
+          * {
+            margin: 0;
+            padding: 0;
+          }
 
-					h1 {
-						font-size: 3rem;
-						margin-bottom: 2rem;
-					}
+          body {
+            font-family: 'Bubblegum Sans', sans-serif;
+            text-align: center;
+          }
 
-					.header {
-						padding: 30px 60px;
-					}
+          h1 {
+            font-size: 3rem;
+            margin-bottom: 2rem;
+          }
 
-					.masonry {
-						margin: 0 auto;
-						max-width: 1280px;
-						padding: 0 60px;
-						text-align: center;
-					}
+          .header {
+            padding: 30px 60px;
+          }
 
-					.masonry img {
-						margin: 10px;
-					}
+          .masonry {
+            margin: 0 auto;
+            max-width: 1280px;
+            padding: 0 60px;
+            text-align: left;
+          }
 
-					.chicken-waffles {
-						margin-bottom: 30px;
-					}
+          .masonry img {
+            margin: 10px;
+            display: block;
+          }
 
-					.chicken {
-						animation: spin linear 3s infinite;
-						margin: 2rem;
-					}
+          .chicken-waffles {
+            margin-bottom: 30px;
+          }
 
-					.waffle {
-						animation: spin-counter linear 6s infinite;
-						margin: 2rem;
-					}
+          .chicken {
+            animation: spin linear 3s infinite;
+            margin: 2rem;
+          }
 
-					@keyframes spin {
-		 				from { transform:rotate(0deg); }
-		    			to   { transform:rotate(360deg); }
-					}
+          .waffle {
+            animation: spin-counter linear 6s infinite;
+            margin: 2rem;
+          }
 
-					@keyframes spin-counter {
-		 				from { transform:rotate(360deg); }
-		    			to   { transform:rotate(0deg); }
-					}
-				`}</style>
-			</div>
-		)
-	}
+          @keyframes spin {
+            from {
+              transform: rotate(0deg);
+            }
+            to {
+              transform: rotate(360deg);
+            }
+          }
+
+          @keyframes spin-counter {
+            from {
+              transform: rotate(360deg);
+            }
+            to {
+              transform: rotate(0deg);
+            }
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
